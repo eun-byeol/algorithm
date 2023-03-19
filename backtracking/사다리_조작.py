9b672c92413bcf9a7dd4f664d8cc7bbf746f3cc1
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-03-20 01:46:43 +0900

    refactor: 사다리 조작 솔루션 개선
    백준 15684번
    사다리가 1~3개인 경우의 수 한번에 계산
    dfs 종료 조건 추가(불필요한 케이스 검증 제거)
    python3으로는 시간초과 나옴

--- backtracking/사다리_조작.py
+++ backtracking/사다리_조작.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+INF = 1e9
 
 N, M, H = map(int, input().split())
 bridge = [[0] * N for _ in range(H+1)]
@@ -11,22 +12,24 @@ def simul(bridge):
   for h in range(1, N+1):
     cur = h
     for i in range(1, H+1):
-      if cur != 1:
+      if cur > 1:
         if bridge[i][cur-1]:
           cur -= 1
           continue
-      if cur != N:
+      if cur < N:
         if bridge[i][cur]:
           cur += 1
     if cur != h:
       return False
   return True
 
-def dfs(depth, bridge, size, at_i, at_j):
+def dfs(depth, bridge, max_h, at_i, at_j):
   global result
-  if depth == size:
-    if simul(bridge):
-      result = depth
+  if depth >= result:
+    return
+  if simul(bridge):
+    result = depth
+  if depth == max_h:
     return
   for i in range(at_i, H+1):
     if i != at_i:
@@ -38,16 +41,12 @@ def dfs(depth, bridge, size, at_i, at_j):
         continue
       if j+1 < N and bridge[i][j+1]:
         continue
-      bridge[i][j] = 2
-      dfs(depth + 1, bridge, size, i, j+1)
+      bridge[i][j] = 1
+      dfs(depth + 1, bridge, max_h, i, j+2)
       bridge[i][j] = 0
 
-result = -1
-if simul(bridge):
-  result = 0
-else:
-  for i in range(1, 4):
-    dfs(0, bridge, i, 1, 1)
-    if result > 0:
-      break
+result = INF
+dfs(0, bridge, 3, 1, 1)
+if result == INF:
+  result = -1
 print(result)
