23a8125262af436bfc02c74b84d047362783d9cc
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-03-20 13:12:05 +0900

    refactor: 치킨 배달 솔루션 가독성 개선
    백준 15686번
    조합 라이브러리 사용하여 가독성 개선

--- backtracking/치킨_배달.py
+++ backtracking/치킨_배달.py
@@ -1,26 +1,7 @@
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 INF = 1e9
-
-def simul(chicken):
-  total = 0
-  for x,y in house:
-    dist = INF
-    for r,c in chicken:
-      dist = min(dist, abs(x-r) + abs(y-c))
-    total += dist
-  return total
-
-def dfs(left_ck, at):
-  global result
-  if len(left_ck) == M:
-    result = min(result, simul(left_ck))
-    return
-  for i in range(at, C):
-    x, y = chicken[i]
-    left_ck.append((x,y))
-    dfs(left_ck, i+1)
-    left_ck.remove((x,y))
 
 N, M = map(int, input().split())
 data = [list(map(int, input().split())) for _ in range(N)]
@@ -32,8 +13,15 @@ for i in range(N):
       house.append((i,j))
     elif data[i][j] == 2:
       chicken.append((i,j))
-C = len(chicken)
 
 result = INF
-dfs([], 0) #남겨둘 치킨집 뽑기
+for combi in combinations(chicken, M):
+  total = 0
+  for x,y in house:
+    dist = INF
+    for r,c in combi:
+      dist = min(dist, abs(x-r) + abs(y-c))
+    total += dist
+  result = min(result, total)
+
 print(result)
