63f9c9cb4da1917c500d0b2fd93c63029202730e
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-03-23 22:17:07 +0900

    feat: 원판 돌리기 솔루션
    백준 17822번
    bfs 사용하여 구현

---
+++ implementation/원판_돌리기.py
@@ -0,0 +1,85 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M, T = map(int, input().split())
+graph = [deque(list(map(int, input().split()))) for _ in range(N)]
+command = []
+for _ in range(T):
+    xi, di, ki = map(int, input().split())
+    command.append((xi, di, ki))
+
+
+def cycle(x, d, k):
+    for i in range(N):
+        if (i+1) % x != 0:
+            continue
+        if d == 0: 
+            graph[i].rotate(k)
+        elif d == 1:
+            graph[i].rotate(-k)
+
+def update():
+    sum = 0
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            sum += graph[i][j]
+            if graph[i][j] != 0:
+                cnt += 1
+    if cnt == 0:
+        return
+    average = sum / cnt
+
+    for i in range(N):
+        for j in range(M):
+            if 0 < graph[i][j] < average: 
+                graph[i][j] += 1
+            elif graph[i][j] > average: 
+                graph[i][j] -= 1
+
+def bfs(a, b, visited):
+    q = deque([ (a, b) ])
+    v = graph[a][b]
+    cnt = 0
+    while q:
+        r, c = q.popleft()
+        visited[r][c] = 1
+
+        for i in range(4):
+            nr = r + dr[i]
+            nc = (c + dc[i]) % M
+            if nr < 0 or nr >= N or visited[nr][nc]: 
+                continue
+            if graph[nr][nc] != 0 and graph[nr][nc] == v:
+                cnt += 1
+                graph[nr][nc] = 0
+                visited[nr][nc] = 1
+                q.append((nr, nc))
+    if cnt > 0 :
+        graph[a][b] = 0
+        return True
+    return False
+    
+
+def delete():
+    visited = [[0] * M for _ in range(N)]
+    delete_flag = 0
+    for i in range(N):
+        for j in range(M):
+            if bfs(i, j, visited):
+                delete_flag = 1
+    if not delete_flag:
+        return update()
+
+dr = [1, -1, 0, 0]
+dc = [0, 0, 1, -1]
+
+for x, d, k in command:
+    cycle(x, d, k)
+    delete()
+
+result = 0
+for i in range(N):
+    result += sum(graph[i])
+print(result)
