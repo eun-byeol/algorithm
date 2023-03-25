0bf17f38e05d9860c327ca09e6b12ec2570b52ae
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-03-25 18:20:07 +0900

    refactor: 청소년 상어 솔루션 개선
    pool에 상어 위치를 -1로 저장하지 않고 모두 0으로 처리, 상어 위치는 shark_x, shark_y로 저장
    삭제된 물고기는 방향을 -1로 변경. (fish[num][2] = -1)
    백준 19236번

--- backtracking/청소년_상어.py
+++ backtracking/청소년_상어.py
@@ -1,41 +1,36 @@
 import sys
+import copy
 input = sys.stdin.readline
 
 N = 4
-data = [list(map(int, input().split())) for _ in range(N)]
 pool = [[0] * N for _ in range(N)]
-fish = [[] * 3 for _ in range(N*N+1)]
+fish = [[0,0,0] for _ in range(N*N+1)]
+
 for i in range(N):
+    data = list(map(int, input().split()))
     for j in range(0, 2*N, 2):
-        n = data[i][j]
-        d = data[i][j+1] - 1
+        n = data[j]
+        d = data[j+1] - 1
         r = i
         c = j // 2
         fish[n] = [r, c, d]
-        pool[r][c] = n    
+        pool[r][c] = n
 
-def fish_move(fish, origin):
-    pool = [p[:] for p in origin]
+def fish_move(fish, pool, shark_x, shark_y):
     for n in range(1, N*N+1):
-        if fish[n][0] == -1: # 삭제된 경우
+        if fish[n][2] == -1: # 삭제된 경우 d = -1
             continue
         r, c, d = fish[n]
         for i in range(8):
             nd = (d+i) % 8
             nr = r + dr[nd]
             nc = c + dc[nd]
-            if 0 <= nr < N and 0 <= nc < N and pool[nr][nc] != -1:
-                tmp = pool[nr][nc]
-                pool[r][c] = tmp
-                pool[nr][nc] = n
-                fish[n][0] = nr
-                fish[n][1] = nc
-                fish[n][2] = nd
-                if tmp > 0:
-                    fish[tmp][0] = r
-                    fish[tmp][1] = c
+            if 0 <= nr < N and 0 <= nc < N and (nr != shark_x or nc != shark_y):
+                nn = pool[nr][nc]
+                pool[r][c], pool[nr][nc] = pool[nr][nc], pool[r][c]
+                fish[n][0], fish[n][1], fish[n][2] = nr, nc, nd
+                fish[nn][0], fish[nn][1] = r, c
                 break
-    return pool
 
 def shark_move(r, c, d, pool):
     edible = []
@@ -45,36 +40,30 @@ def shark_move(r, c, d, pool):
         if r < 0 or r >= N or c < 0 or c >= N:
             return edible
         if pool[r][c] != 0:
-            edible.append((pool[r][c], r, c))
+            edible.append(pool[r][c])
     return edible
 
-def dfs(origin, fish_o, total, r, c, d):
-    fish = [f[:] for f in fish_o]
-    pool = fish_move(fish, origin)
-    edible = shark_move(r, c, d, pool)
+def dfs(pool, fish, total, num):
+    global result
+    shark_x, shark_y, shark_d = fish[num]
+    total += num
+    pool[shark_x][shark_y] = 0
+    fish[num][2] = -1 # 삭제된 경우 d = -1
+
+    fish_move(fish, pool, shark_x, shark_y)
+    edible = shark_move(shark_x, shark_y, shark_d, pool)
     if len(edible) == 0:
         result = max(result, total)
         return
-
     for e in edible:
-        num, nr, nc = e
-        nd = fish[num][2]
-        pool[nr][nc] = -1 # 상어 위치
-        pool[r][c] = 0
-        fish[num][0] = -1 # 정보 삭제
-        dfs(pool, fish, total + num, nr, nc, nd)
-        pool[nr][nc] = num
-        pool[r][c] = -1 # 상어 원위치
-        fish[num][0] = nr
+        dfs(copy.deepcopy(pool), copy.deepcopy(fish), total, e)
 
 dr = [-1, -1, 0, 1, 1, 1, 0, -1] # 반시계
 dc = [0, -1, -1, -1, 0, 1, 1, 1]
 
 num = pool[0][0]
-shark = [0, 0, fish[num][2]] # r, c, d
-fish[num][0] = -1
-pool[0][0] = -1 #상어가 있는 위치
+d = fish[num][2]
 
 result = 0
-dfs(pool, fish, num, 0, 0, fish[num][2])
+dfs(pool, fish, 0, pool[0][0])
 print(result)
