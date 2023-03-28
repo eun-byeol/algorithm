9be90fc0ded4041596c6d5f1dc284ad3d077fc46
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-03-28 12:42:49 +0900

    refactor: 주사위 윷놀이 솔루션 시간복잡도 개선
    itertools 라이브러리 대신, dfs로 구현
    백준 17825번

--- backtracking/주사위_윷놀이.py
+++ backtracking/주사위_윷놀이.py
@@ -8,30 +8,33 @@ dice = list(map(int, input().split()))
 index = [[1], [2], [3], [4], [5], [6, 21], [7], [8], [9], [10], [11, 27], [12], [13], [14], [15], [16, 29], [17], [18], [19], [20], [-1], [22], [23], [24], [25], [26], [20], [28], [24], [30], [31], [24]]
 score = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 13, 16, 19, 25, 30, 35, 22, 24, 28, 27, 26, 0]
 
-def calculate(p, dice):
-    total = 0
-    position = [0, 0, 0, 0, 0]
-    for i in range(T):
-        horse = p[i]
-        number = dice[i]
+
+def dfs(depth, total, position):
+    global result
+    if depth == T:
+        result = max(result, total)        
+        return
+    for horse in range(1, 5):
+        number = dice[depth]
         cur = position[horse]
-        if cur == -1: # 도착한 말을 꺼내는 경우 없음
-            return 0
-        if cur == 5 or cur == 10 or cur == 15: # 출발이 파란색 지점일 때, 1칸 먼저 이동
+        tmp = cur
+        if cur == -1:
+            continue
+        if cur == 5 or cur == 10 or cur == 15:
             cur = index[cur][1]
             number -= 1
-        for _ in range(number): # 이동하기
+        for _ in range(number):
             cur = index[cur][0]
-            if cur == -1: #도착한 경우
+            if cur == -1:
                 break
-        if cur != -1 and cur in position: # 말이 겹치는 경우 없음
-            return 0
-        total += score[cur]
+        if cur != -1 and cur in position:
+            continue
         position[horse] = cur
-    return total
+        dfs(depth+1, total + score[cur], position)
+        position[horse] = tmp
 
 data = [1, 2, 3, 4]
 result = 0
-for p in list(product(data, repeat=T)):
-    result = max(calculate(p, dice), result)
+position = [0, 0, 0, 0, 0]
+dfs(0, 0, position)
 print(result)
