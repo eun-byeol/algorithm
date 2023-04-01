e460f90cb38e727559afbe6f1a290564f59ec670
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-01 17:28:12 +0900

    refactor: 마법사 상어와 파이어볼 솔루션 개선
    파이어볼을 저장하는 리스트 자료구조 추가, 불필요한 tmp 메모리 제거, sum 함수 가독성 개선
    백준 20056번

--- implementation/마법사_상어와_파이어볼.py
+++ implementation/마법사_상어와_파이어볼.py
@@ -3,65 +3,54 @@ input = sys.stdin.readline
 
 N, M, k = map(int, input().split())
 graph = [[[] for _ in range(N)] for _ in range(N)]
+fireballs = []
 for _ in range(M):
     r, c, m, s, d = map(int, input().split())
-    graph[r-1][c-1].append([m, s, d])
+    fireballs.append([r, c, m, s, d])
 
-def move(graph):
-    tmp = [[[] for _ in range(N)] for _ in range(N)]
-    for r in range(N):
-        for c in range(N):
-            for m, s, d in graph[r][c]:
-                nr = (N + r + (dr[d]*s)%N) % N
-                nc = (N + c + (dc[d]*s)%N) % N
-                tmp[nr][nc].append([m, s, d])
-    return tmp
+def move(graph, fireballs):
+    while fireballs:
+        r, c, m, s, d = fireballs.pop()
+        nr = (N + r + (dr[d]*s)%N) % N
+        nc = (N + c + (dc[d]*s)%N) % N
+        graph[nr][nc].append([m, s, d])
 
-def divide(graph, count):
+def divide(graph, fireballs):
     for r in range(N):
         for c in range(N):
             cnt = len(graph[r][c])
-            if cnt <= 1:
-                continue
-            sum_of_m = 0
-            sum_of_s = 0
-            dir_flag = [0, 0] #홀, 짝
-            while graph[r][c]:
-                m, s, d = graph[r][c].pop()
-                sum_of_m += m
-                sum_of_s += s
-                if d % 2 == 0:
-                    dir_flag[1] = 1
+            if cnt == 1:
+                fireballs.append([r, c] + graph[r][c].pop())
+            elif cnt > 1:
+                sum_of_m, sum_of_s = 0, 0
+                dir_flag = [0, 0] #홀짝
+                while graph[r][c]:
+                    m, s, d = graph[r][c].pop()
+                    sum_of_m += m
+                    sum_of_s += s
+                    if d % 2 == 0:
+                        dir_flag[1] = 1
+                    else:
+                        dir_flag[0] = 1
+                next_m = int(sum_of_m / 5)
+                if next_m == 0:
+                    continue
+                next_s = int(sum_of_s / cnt)
+                if sum(dir_flag) == 1:
+                    next_d = [0, 2, 4, 6]
                 else:
-                    dir_flag[0] = 1
-            next_m = int(sum_of_m / 5)
-            if next_m == 0:
-                count -= cnt
-                continue
-            count += 4 - cnt
-            next_s = int(sum_of_s / cnt)
-            if sum(dir_flag) == 1:
-                next_d = [0, 2, 4, 6]
-            else:
-                next_d = [1, 3, 5, 7]
-            for i in range(4):
-                graph[r][c].append([next_m, next_s, next_d[i]])
-    return count
-    
+                    next_d = [1, 3, 5, 7]
+                for nd in next_d:
+                    fireballs.append([r, c, next_m, next_s, nd])
+        
 dr = [-1, -1, 0, 1, 1, 1, 0, -1]
 dc = [0, 1, 1, 1, 0, -1, -1, -1]
 
-count = M
 for _ in range(k):
-    if count == 0:
+    if len(fireballs) == 0:
         break
-    graph = move(graph)
-    count = divide(graph, count)
+    move(graph, fireballs)
+    divide(graph, fireballs)
 
-result = 0
-if count != 0:
-    for r in range(N):
-        for c in range(N):
-            for fireball in graph[r][c]:
-                result += fireball[0]
+result = sum([f[2] for f in fireballs])
 print(result)
