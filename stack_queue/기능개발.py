7154fbf40ee5e4a135a3f7886ce0b527f927915a
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-04 14:43:18 +0900

    refactor: 기능개발 솔루션 개선
    math.ceil 메서드 대신 음수 활용
    스택 활용
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/42586

--- stack_queue/기능개발.py
+++ stack_queue/기능개발.py
@@ -1,17 +1,9 @@
-import math
 def solution(progresses, speeds):
     answer = []
-    days = []
     for prg, spd in zip(progresses, speeds):
-        days.append(math.ceil((100 - prg) / spd))
-        
-    release = 0
-    cnt = 0
-    for i in range(len(days)):
-        if days[i] > days[release]:
-            answer.append(cnt)
-            release = i
-            cnt = 0
-        cnt += 1
-    answer.append(cnt)
-    return answer
+        day = -((prg - 100) // spd)
+        if len(answer) == 0 or answer[-1][0] < day:
+            answer.append([day, 1])
+        else:
+            answer[-1][1] += 1
+    return [a[1] for a in answer]
