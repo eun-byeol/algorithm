811fc21225a6a651e18c23a67f5b5f4fd87542d3
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-04 13:00:08 +0900

    refactor: 전화번호 목록 솔루션 개선
    hash map 이용한 풀이 추가
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3

--- hash/전화번호_목록.py
+++ hash/전화번호_목록.py
@@ -5,3 +5,18 @@ def solution(phone_book):
         if phone_book[i+1].startswith(phone_book[i]):
             return False
     return True
+
+
+#hash map 이용 풀이
+
+def solution(phone_book):
+    dict = {}
+    for numbers in phone_book:
+        dict[numbers] = 1
+    for numbers in phone_book:
+        tmp = ""
+        for num in numbers:
+            tmp += num
+            if tmp in dict and tmp != numbers:
+                return False
+    return True
