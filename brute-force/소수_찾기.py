679460bccc24e12bfc3a5f57bb423802ba5960f0
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-05 18:00:31 +0900

    refactor: 소수 찾기 솔루션 개선
    소수 검증 함수 범위 sqrt(n)까지로 수정
    리스트를 문자열로 합치는 것을 join 함수를 사용하여 수정
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/42839

--- brute-force/소수_찾기.py
+++ brute-force/소수_찾기.py
@@ -1,24 +1,21 @@
 from itertools import permutations
-def is_valid(number):
+def is_prime(number):
     if number == 0 or number == 1:
         return False
-    for n in range(2, number):
+    for n in range(2, int(number ** 0.5) + 1):
         if number % n == 0:
             return False
     return True
 
 def solution(numbers):
     answer = 0
-    result = set()
+    primes = set()
     for i in range(1, len(numbers)+1):
-        for permu in permutations(numbers, i):
-            tmp = ""
-            for num in permu:
-                tmp += num
-            number = int(tmp)
-            if number in result:
+        for p in permutations(numbers, i):
+            num = int(''.join(p))
+            if num in primes:
                 continue
-            if is_valid(number):
-                result.add(number)
+            if is_prime(num):
+                primes.add(num)
                 answer += 1
     return answer
