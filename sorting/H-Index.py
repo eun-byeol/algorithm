b4d6279dacda5de8b93f453bd9b932d93f2b6ac7
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-07 14:40:09 +0900

    refactor: H-Index 솔루션 개선
    오름차순 버전 솔루션 추가
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/42747

--- sorting/H-Index.py
+++ sorting/H-Index.py
@@ -1,3 +1,4 @@
+# ver. 오름차순
 def solution(citations):
     citations.sort()
     length = len(citations)
@@ -6,3 +7,12 @@ def solution(citations):
         if c >= cnt:
             return cnt
     return 0
+
+# ver. 내림차순
+def solution(citations):
+    citations.sort(reverse=True)
+    length = len(citations)
+    for i, c in enumerate(citations):
+        if i >= c:
+            return i
+    return length
