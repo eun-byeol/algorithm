12249617a63d093be80680a7d12e4a6b858066a6
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-07 15:42:10 +0900

    refactor: 위장 솔루션 개선
    Counter 라이브러리 사용하여 가독성 개선
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/42578

--- hash/위장.py
+++ hash/위장.py
@@ -1,7 +1,6 @@
+from collections import Counter
 def solution(clothes):
-    cnt = {}
-    for name, kind in clothes:
-        cnt[kind] = cnt.get(kind, 0) + 1
+    cnt = Counter([kind for name, kind in clothes])
     answer = 1
     for key in cnt:
         answer *= (cnt[key] + 1)
