82fbba584531756a8b346fe44cf2f1aab42d254d
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-11 22:10:36 +0900

    refactor: 순위 솔루션 개선
    set 딕셔너리 자료형으로 수정
    승 개수 + 패 개수 = n-1 이면 순위 확정 가능
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/49191

--- graph/순위.py
+++ graph/순위.py
@@ -1,3 +1,4 @@
+# ver. bfs
 from collections import deque
 def bfs(idx, value, total, graph, n):
     visited = [0] * n
@@ -21,3 +22,25 @@ def solution(n, results):
         bfs(i, 1, total, graph, n)
         bfs(i, -1, total, graph, n)
     return total.count(n-1)
+
+# ver. set 딕셔너리
+from collections import defaultdict
+def solution(n, results):
+    answer = 0
+    win = defaultdict(set)
+    lose = defaultdict(set)
+    
+    for w, l in results:
+        win[w].add(l) #한 개 추가
+        lose[l].add(w)
+    
+    for num in range(1, n+1):
+        for w in lose[num]:
+            win[w].update(win[num]) #여러개 추가
+        for l in win[num]:
+            lose[l].update(lose[num])
+    
+    for num in range(1, n+1):
+        if len(win[num]) + len(lose[num]) == n-1:
+            answer += 1
+    return answer
