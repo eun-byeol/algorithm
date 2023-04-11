a7f3c5ec914ff4b7937419acdfdc1c8cd9438715
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-11 21:06:09 +0900

    refactor: 등굣길 솔루션 개선
    bfs대신, (1~n, 1~m)까지 순차적으로 접근하는 방식으로 수정하여 가독성 개선.
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/42898

--- dp/등굣길.py
+++ dp/등굣길.py
@@ -1,28 +1,19 @@
-from collections import deque
-def bfs(graph, n, m):
-    visited = [[0] * m for _ in range(n)]
-    q = deque([(0,0)])
-    graph[0][0] = 1
-    visited[0][0] = 1
-    while q:
-        r, c = q.popleft()
-        if r-1 >= 0 and graph[r-1][c] != -1:
-            graph[r][c] += graph[r-1][c] % 1000000007
-        if c-1 >= 0 and graph[r][c-1] != -1:
-            graph[r][c] += graph[r][c-1] % 1000000007
-        if r+1 < n and graph[r+1][c] != -1 and not visited[r+1][c]:
-            visited[r+1][c] = 1
-            q.append((r+1, c))
-        if c+1 < m and graph[r][c+1] != -1 and not visited[r][c+1]:
-            visited[r][c+1] = 1
-            q.append((r, c+1))
-    return graph[n-1][m-1] % 1000000007
-        
 def solution(m, n, puddles):
-    graph = [[0] * m for _ in range(n)]
+    graph = [[0] * (m+1) for _ in range(n+1)]
     for p in puddles:
         if len(p) == 0:
             break
-        graph[p[1]-1][p[0]-1] = -1
-    answer = bfs(graph, n, m)
-    return answer
+        y, x = p
+        graph[x][y] = -1
+        
+    graph[1][1] = 1
+        
+    for r in range(1, n+1):
+        for c in range(1, m+1):
+            if graph[r][c] == -1:
+                continue
+            if graph[r-1][c] != -1:
+                graph[r][c] += graph[r-1][c]
+            if graph[r][c-1] != -1:
+                graph[r][c] += graph[r][c-1]
+    return graph[n][m] % 1000000007
