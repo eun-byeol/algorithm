a2cadbbae2c161579e6e373940896148f15840a5
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-12 20:31:35 +0900

    refactor: 아이템 줍기 솔루션 개선
    길을 -1로 수정
    bfs에서 큐에 dist를 저장하는 것 대신, graph에 직접 dist값 저장
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/87694

--- bfs/아이템_줍기.py
+++ bfs/아이템_줍기.py
@@ -2,28 +2,28 @@ from collections import deque
 def bfs(cx, cy, ix, iy, graph):
     dr = [0, 0, 1, -1]
     dc = [1, -1, 0, 0]
-    q = deque([(cx, cy, 0)])
-    graph[cx][cy] = 2
+    q = deque([(cx, cy)])
+    graph[cx][cy] = 0
     while q:
-        r, c, dist = q.popleft()
+        r, c = q.popleft()
         if r == ix and c == iy:
-            return dist // 2
+            return graph[r][c] // 2
         for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
-            if 0 < nr < 102 and 0 < nc < 102 and graph[nr][nc] == 1:
-                graph[nr][nc] = 2
-                q.append((nr, nc, dist+1))
+            if 0 < nr < 102 and 0 < nc < 102 and graph[nr][nc] == -1:
+                graph[nr][nc] = graph[r][c] + 1
+                q.append((nr, nc))
     return -1
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
     graph = [[0] * 102 for _ in range(102)]
-
+    
     for x1, y1, x2, y2 in rectangle:
         for r in range(x1*2, x2*2+1):
             for c in range(y1*2, y2*2+1):
-                graph[r][c] = 1
-
+                graph[r][c] = -1
+                
     for x1, y1, x2, y2 in rectangle:
         for r in range(x1*2+1, x2*2):
             for c in range(y1*2+1, y2*2):
