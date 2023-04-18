02b767948c60899a13cfc7d77e316aba555124ce
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-19 00:18:00 +0900

    refactor: 아방가르드 타일링 솔루션 개선
    4~6까지 하드코딩 된 부분 수정
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/181186

--- dp/아방가르드_타일링.py
+++ dp/아방가르드_타일링.py
@@ -1,8 +1,14 @@
 def solution(n):
     dp = [0] * 100001
-    dp[1:7] = [1, 3, 10, 23, 62, 170]
+    dp[1:4] = [1, 3, 10]
+    s = [0, 1, 2, 5, 2, 2, 4]
     mod = 1000000007
-
-    for i in range(7, n+1):
+    
+    for i in range(4, 7): # 4 ~ 6까지 구하기
+        for j in range(1, i):
+            dp[i] += s[j] * dp[i-j]
+        dp[i] += s[i]
+    
+    for i in range(7, n+1): # 7이상 구하기
         dp[i] = (dp[i-1] + 2*dp[i-2] + 6*dp[i-3] + dp[i-4] - dp[i-6]) % mod
     return dp[n]
