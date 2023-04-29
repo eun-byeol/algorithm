f4639e4441a8eae9217be662882552b12d0563f2
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-04-29 22:48:56 +0900

    refactor: 디펜스 게임 솔루션 개선
    최소힙 사용하는 방식으로 개선
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/142085

--- greedy/디펜스_게임.py
+++ greedy/디펜스_게임.py
@@ -1,23 +1,10 @@
 import heapq
 def solution(n, k, enemy):
-    answer = 0
-    max_heap = []
+    hp = enemy[:k]
+    heapq.heapify(hp)
     
-    i = 0
-    while i < len(enemy):
-        e = enemy[i]
-        if k == 0 and n < e:
-            break
-        if n < e:
-            if len(max_heap) > 0 and -max_heap[0] > e:
-                n += -heapq.heappop(max_heap)
-                i -= 1
-            else:
-                answer += 1
-            k -= 1
-        else:
-            n -= e
-            heapq.heappush(max_heap, -e)
-            answer += 1
-        i += 1
-    return answer
+    for i in range(k, len(enemy)):
+        n -= heapq.heappushpop(hp, enemy[i])
+        if n < 0:
+            return i
+    return len(enemy)
