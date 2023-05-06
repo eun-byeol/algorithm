77a0b548c20c50003c267a6127590509bb609d7c
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-05-06 23:17:49 +0900

    refactor: 연속 부분 수열 합의 개수 솔루션 개선
    sum() 메서드를 대체하여 시간복잡도 개선
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/131701

--- implementation/연속 _부분_수열_합의_개수.py
+++ implementation/연속 _부분_수열_합의_개수.py
@@ -1,11 +1,11 @@
 def solution(elements):
     N = len(elements)
-    sum_v = set(elements)
-    for n in range(2, N):
-        for i in range(N):
-            v = sum(elements[i:i+n])
-            if i+n > N:
-                v += sum(elements[:(i+n)%N])
+    sum_v = set()
+    
+    for i in range(N):
+        v = elements[i]
+        sum_v.add(v)
+        for j in range(i+1, i+N):
+            v += elements[j%N]
             sum_v.add(v)
-    sum_v.add(sum(elements))
     return len(sum_v)
