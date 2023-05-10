0e44e94e8658fc9021e12078a4225d69f05f855a
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-05-10 22:08:42 +0900

    feat: 등산코스 정하기 솔루션
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/118669

---
+++ dijkstra/등산코스_정하기.py
@@ -0,0 +1,48 @@
+import heapq
+INF = int(1e9)
+N = 0
+graph = []
+
+def make_graph(paths):
+    global graph
+    graph = [[] for _ in range(N+1)]
+    for v1, v2, e in paths:
+        graph[v1].append((v2, e))
+        graph[v2].append((v1, e))
+
+def dijkstra(gates, summits):
+    intensity = [INF] * (N+1)
+    visited = [0] * (N+1)
+    h = []
+    for gate in gates:
+        intensity[gate] = 0
+        heapq.heappush(h, (0, gate))
+        
+    while h:
+        cur_e, cur = heapq.heappop(h)
+        visited[cur] = 1
+        if cur in summits or cur_e > intensity[cur]:
+            continue
+            
+        for v, e in graph[cur]:
+            if visited[v] == 1:
+                continue
+            max_e = max(e, cur_e)
+            if max_e < intensity[v]:
+                intensity[v] = max_e
+                heapq.heappush(h, (max_e, v))
+    return intensity
+        
+def solution(n, paths, gates, summits):
+    global N
+    answer = [INF, INF]
+    N = n
+    make_graph(paths)
+    intensity = dijkstra(gates, set(summits))
+    
+    summits.sort()
+    for summit in summits:
+        if intensity[summit] < answer[1]:
+            answer[1] = intensity[summit]
+            answer[0] = summit
+    return answer
