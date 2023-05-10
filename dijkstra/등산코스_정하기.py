e8c2c6786ed3e0dac876678a4336192ac655452e
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-05-11 01:53:03 +0900

    refactor: 등산코스 정하기 솔루션 개선 및 추가
    bfs + queue으로 코드 가독성 향상
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/118669

--- dijkstra/등산코스_정하기.py
+++ dijkstra/등산코스_정하기.py
@@ -46,3 +46,48 @@ def solution(n, paths, gates, summits):
             answer[1] = intensity[summit]
             answer[0] = summit
     return answer
+
+# ver. bfs
+from collections import deque
+INF = int(1e9)
+graph = []
+N = 0
+
+def bfs(q, visited, gates, summits):
+    answer = [INF, INF]
+    summits_set = set(summits)
+    while q:
+        e, v = q.popleft()
+        if e > answer[1]:
+            continue
+        if v in summits_set:
+            if e < answer[1] or (e == answer[1] and v < answer[0]):
+                answer = [v, e]
+            continue
+        for next_v, next_e in graph[v]:
+            max_e = max(next_e, e)
+            if visited[next_v] == -1:
+                continue
+            if 0 < visited[next_v] <= max_e:
+                continue
+            q.append((max_e, next_v))
+            visited[next_v] = max_e
+    return answer
+
+def make_graph(paths):
+    global graph
+    graph = [[] for _ in range(N+1)]
+    for v1, v2, e in paths:
+        graph[v1].append((v2, e))
+        graph[v2].append((v1, e))
+        
+def solution(n, paths, gates, summits):
+    global N
+    N = n
+    make_graph(paths)
+    q = deque()
+    visited = [0] * (N+1)
+    for gate in gates:
+        q.append((0, gate))
+        visited[gate] = -1
+    return bfs(q, visited, gates, summits)
