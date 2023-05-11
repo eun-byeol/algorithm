48ef62b41de68e1a8f03a639d264fb8350a071a1
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-05-11 12:52:18 +0900

    refactor: 이진 변환 반복하기 솔루션 개선
    이진수 특징 사용하여 솔루션 가독성 개선
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/70129

--- implementation/이진_변환_반복하기.py
+++ implementation/이진_변환_반복하기.py
@@ -1,12 +1,8 @@
 def solution(s):
     turn, zero = 0, 0
     while s != "1":
-        tmp = ""
-        for v in s:
-            if v == "1":
-                tmp += v
-            else:
-                zero += 1
-        s = format(len(tmp), 'b')
+        n = s.count('1')
+        zero += len(s) - n
+        s = format(n, 'b')
         turn += 1
     return [turn, zero]
