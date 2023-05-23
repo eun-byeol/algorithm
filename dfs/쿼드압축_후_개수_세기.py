52dc3922620d51a910c0108ce499d90708725174
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-05-23 10:48:51 +0900

    refactor: 쿼드압축 후 개수 세기 솔루션 개선
    dfs에 answer 인자 추가하여, 개수 세는 로직 단순화
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/68936

--- dfs/쿼드압축_후_개수_세기.py
+++ dfs/쿼드압축_후_개수_세기.py
@@ -1,28 +1,20 @@
-def dfs(n, x, y, arr):
+def dfs(n, x, y, arr, answer):
     if n == 1:
+        answer[arr[x][y]] += 1
         return
     for i in range(x, x+n):
         for j in range(y, y+n):
             if arr[x][y] != arr[i][j]:
                 nn = n//2
-                dfs(nn, x, y, arr)
-                dfs(nn, x, y+nn, arr)
-                dfs(nn, x+nn, y, arr)
-                dfs(nn, x+nn, y+nn, arr)
+                dfs(nn, x, y, arr, answer)
+                dfs(nn, x, y+nn, arr, answer)
+                dfs(nn, x+nn, y, arr, answer)
+                dfs(nn, x+nn, y+nn, arr, answer)
                 return
-    for i in range(x, x+n):
-        for j in range(y, y+n):
-            if i == x and j == y: continue
-            arr[i][j] = -1
+    answer[arr[x][y]] += 1
     
 def solution(arr):
-    N = len(arr)
-    dfs(N, 0, 0, arr)
     answer = [0,0]
-    for a in arr:
-        for i in range(N):
-            if a[i] == 0:
-                answer[0] += 1
-            elif a[i] == 1:
-                answer[1] += 1
+    N = len(arr)
+    dfs(N, 0, 0, arr, answer)
     return answer
