191b36731d534db95943d650f060b91f3cc1ee74
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-06-02 14:24:19 +0900

    feat: [3차] n진수 게임 솔루션
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/17687

---
+++ implementation/[3차]_n진수_게임.py
@@ -0,0 +1,20 @@
+num_str = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+
+def transform(num, n):
+    if num <= 1:
+        return num_str[num]
+    if num // n == 0:
+        return num_str[num % n]
+    return transform(num // n, n) + num_str[num % n]
+
+def solution(n, t, m, p):
+    answer = ''
+    nums = ''
+    next_num = 0
+    for i in range(t):
+        idx = p-1 + (i * m)
+        while len(nums)-1 < idx:
+            nums += transform(next_num, n)
+            next_num += 1
+        answer += nums[idx]
+    return answer
