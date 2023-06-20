6a933c5be1f4b2fe0986b78037e09f5c4030ff9a
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-06-20 14:31:38 +0900

    refactor: [1차] 뉴스 클러스터링 솔루션 개선
    다중집합 교집합, 합집합 구하는 로직 Counter 라이브러리 사용하여 간소화
    프로그래머스 https://school.programmers.co.kr/learn/courses/30/lessons/17677

--- implementation/[1차]_뉴스_클러스터링.py
+++ implementation/[1차]_뉴스_클러스터링.py
@@ -1,21 +1,21 @@
+from collections import Counter
+
 def split(s):
     result = []
     for i in range(len(s)-1):
-        if 'a' <= s[i] <= 'z' and 'a' <= s[i+1] <= 'z':
+        if s[i:i+2].isalpha():
             result.append(s[i:i+2])
     return result
 
 def calculate(d1, d2):
-    if len(d1) == 0 and len(d2) == 0:
-        return 1
+    counter1 = Counter(d1)
+    counter2 = Counter(d2)
     
-    union, inter = [], []
-    for v in d1:
-        union.append(v)
-        if v in d2:
-            inter.append(v)
-            d2.remove(v)
-    union.extend(d2)
+    inter = list((counter1 & counter2).elements())
+    union = list((counter1 | counter2).elements())
+    
+    if len(union) == 0:
+        return 1
     return len(inter) / len(union)
             
 def solution(str1, str2):
