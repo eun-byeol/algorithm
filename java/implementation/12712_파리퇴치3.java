21e8256b61846004ffb8a7913742ffb73a1c736e
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-07-06 13:01:06 +0900

    refactor: 12712. 파리퇴치3 솔루션 개선
    simul 함수 중복 제거
    SWEA 12712번 문제

--- java/implementation/12712_파리퇴치3.java
+++ java/implementation/12712_파리퇴치3.java
@@ -2,35 +2,14 @@ import java.util.*;
 import java.lang.*;
 
 class Solution {
-	static int simulByCross(int[][] board, int r, int c, int N, int M) {
-		int[] dr = {-1, 0, 1, 0};
-		int[] dc = {0, 1, 0, -1};
+	static int simulByDir(int[][] board, int r, int c, int N, int M, int[][] dir) {
 		int total = board[r][c];
 		for (int i=0; i<4; i++) {
 			int nr = r;
 			int nc = c;
 			for (int j=0; j<M-1; j++) {
-				nr += dr[i];
-				nc += dc[i];
-				if (nr < 0 || nr >= N || nc < 0 || nc >= N) {
-					break;
-				}
-				total += board[nr][nc];
-			}
-		}
-		return total;
-	}
-	
-	static int simulByDiag(int[][] board, int r, int c, int N, int M) {
-		int[] dr = {-1, -1, 1, 1};
-		int[] dc = {-1, 1, 1, -1};
-		int total = board[r][c];
-		for (int i=0; i<4; i++) {
-			int nr = r;
-			int nc = c;
-			for (int j=0; j<M-1; j++) {
-				nr += dr[i];
-				nc += dc[i];
+				nr += dir[i][0];
+				nc += dir[i][1];
 				if (nr < 0 || nr >= N || nc < 0 || nc >= N) {
 					break;
 				}
@@ -44,6 +23,9 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		int T;
 		T=sc.nextInt();
+		
+		int[][] crossDir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+		int[][] diagDir = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
 
 		for(int test_case = 1; test_case <= T; test_case++) {
 			int N = sc.nextInt();
@@ -57,8 +39,8 @@ class Solution {
 			int ans = 0;
 			for (int i=0; i<N; i++) {
 				for (int j=0; j<N; j++) {
-					ans = Math.max(simulByCross(board, i, j, N, M), ans);
-					ans = Math.max(simulByDiag(board, i, j, N, M), ans);
+					ans = Math.max(simulByDir(board, i, j, N, M, crossDir), ans);
+					ans = Math.max(simulByDir(board, i, j, N, M, diagDir), ans);
 				}
 			}
 			System.out.printf("#%d %d\n", test_case, ans);
