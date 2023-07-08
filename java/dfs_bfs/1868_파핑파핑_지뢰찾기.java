62869971fc1f35aada9b949c61efa8c5984910b8
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-07-08 17:53:29 +0900

    refactor: 1868. 파핑파핑 지뢰찾기 솔루션 개선
    메서드 분리
    큐 구현방법 변경(List<Integer> -> int[])
    SWEA 1868번 문제

--- java/dfs_bfs/1868_파핑파핑_지뢰찾기.java
+++ java/dfs_bfs/1868_파핑파핑_지뢰찾기.java
@@ -19,23 +19,60 @@ class Solution {
 		}
 	}
 	
+	static int[][] simul(String[] inputs, int N, Queue<int[]> q) {
+		int[][] board = new int[N][N];
+    	for (int i=0; i<N; i++) {
+    		for (int j=0; j<N; j++) {
+    			counter(i, j, inputs, N, board);
+    			if (board[i][j] == 0) {
+    				q.add(new int[]{i, j});
+    			}
+    		}
+    	}
+    	return board;
+	}
+	
 	static void bfs(int x, int y, int[][] board, int N, int[][] visited, int cnt) {
-		Queue<List<Integer>> q = new LinkedList<>();
-		q.add(Arrays.asList(x, y));
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[]{x, y});
 		while (!q.isEmpty()) {
-			List<Integer> cur = q.poll();
-			int r = cur.get(0);
-			int c = cur.get(1);
+			int[] idx = q.poll();
+			int r = idx[0];
+			int c = idx[1];
 			for (int i=0; i<8; i++) {
 				int nr = r + dir8[i][0];
 				int nc = c + dir8[i][1];
 				if (nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc] != 0 || board[nr][nc] == -1) continue;
 				if (board[nr][nc] == 0) {
-					q.add(Arrays.asList(nr, nc));
+					q.add(new int[]{nr, nc});
 				}
 				visited[nr][nc] = cnt;
 			}
 		}
+	}
+	
+	static int countGroup(int[][] board, int N, Queue<int[]> q) {
+		int cnt = 0;
+    	int[][] visited = new int[N][N];
+    	
+    	while (!q.isEmpty()) {
+    		int[] idx = q.poll();
+    		int r = idx[0];
+    		int c = idx[1];
+    		if (visited[r][c] == 0) {
+    			visited[r][c] = ++cnt;
+    			bfs(r, c, board, N, visited, cnt);
+    		}
+    	}
+    	
+    	for (int i=0; i<N; i++) {
+    		for (int j=0; j<N; j++) {
+    			if (board[i][j] > 0 && visited[i][j] == 0) {
+    				visited[i][j] = ++cnt;
+    			}
+    		}
+    	}
+    	return cnt;
 	}
 
     public static void main(String args[]) throws Exception {
@@ -46,42 +83,17 @@ class Solution {
         for(int test_case = 1; test_case <= T; test_case++) {
         	int N = sc.nextInt();
         	sc.nextLine();
+        	
         	String[] inputs = new String[N];
         	for (int i=0; i<N; i++) {
         		inputs[i] = sc.nextLine();
         	}
         	
-        	int[][] board = new int[N][N];
-        	Queue<List<Integer>> zeroQ = new LinkedList<>(); 
-        	for (int i=0; i<N; i++) {
-        		for (int j=0; j<N; j++) {
-        			counter(i, j, inputs, N, board);
-        			if (board[i][j] == 0) {
-        				zeroQ.add(Arrays.asList(i, j));
-        			}
-        		}
-        	}
+        	Queue<int[]> q = new LinkedList<>();
+        	int[][] board = simul(inputs, N, q);
+        
+        	int ans = countGroup(board, N, q);
         	
-        	int ans = 0;
-        	int[][] visited = new int[N][N];
-        	
-        	while (!zeroQ.isEmpty()) {
-        		List<Integer> indexes = zeroQ.poll();
-        		int r = indexes.get(0);
-        		int c = indexes.get(1);
-        		if (visited[r][c] == 0) {
-        			visited[r][c] = ++ans;
-        			bfs(r, c, board, N, visited, ans);
-        		}
-        	}
-        	
-        	for (int i=0; i<N; i++) {
-        		for (int j=0; j<N; j++) {
-        			if (board[i][j] > 0 && visited[i][j] == 0) {
-        				visited[i][j] = ++ans;
-        			}
-        		}
-        	}
         	System.out.printf("#%d %d\n", test_case, ans);
         }
     }
