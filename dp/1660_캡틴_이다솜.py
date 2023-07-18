982a9af7ea18d1656aa6214a684c5f1cdd3eaa21
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-07-18 12:12:48 +0900

    refactor: 캡틴 이다솜 솔루션 개선
    dp 초기화 반복문 제거
    백준 https://www.acmicpc.net/problem/1660

--- dp/1660_캡틴_이다솜.py
+++ dp/1660_캡틴_이다솜.py
@@ -1,18 +1,15 @@
 N = int(input())
 
-cnts = [int(n * (n+1) * (2 * n + 1)/12 + n * (n + 1)/4) for n in range(1000)]
-dp = [i for i in range(N+1)]
-i = 1
-while cnts[i] <= N:
-    dp[cnts[i]] = 1
-    i += 1
+data = [int(n * (n+1) * (2 * n + 1)/12 + n * (n + 1)/4) for n in range(1000)]
 
-for i in range(N+1):
-    for j in range(len(cnts)):
-        k = i - cnts[j]
+dp = [i for i in range(N+1)]
+
+for i in range(1, N+1):
+    for num in data:
+        k = i - num
         if k < 0:
             break
         if dp[i] > dp[k] + 1:
-           dp[i] = dp[k] + 1
+            dp[i] = dp[k] + 1
 
 print(dp[N])
