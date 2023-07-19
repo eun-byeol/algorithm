59cc122b04673964d68aa931246445fe98619024
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-07-19 10:07:34 +0900

    refactor: 돌 게임2 솔루션 개선
    dp 풀이로 변경
    백준 https://www.acmicpc.net/problem/9656

--- dp/9656_돌_게임2.py
+++ dp/9656_돌_게임2.py
@@ -1,6 +1,10 @@
 N = int(input())
+dp = [0, 1, -1, 1]
 
-if N % 2 == 0:
-    print("SK")
-else:
-    print("CY")
+for i in range(4, N+1):
+    if dp[i-1] == 1 or dp[i-3] == 1:
+        dp.append(-1)
+    else:
+        dp.append(1)
+
+print("CY") if dp[N] == 1 else print("SK")
