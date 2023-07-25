560ce4552fbc11fbda4ecaa4c5b358c4b113ab35
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-07-25 10:31:50 +0900

    refactor: 1233. 사칙연산 유효성 검사 솔루션 개선
    입력받음과 동시에 유효성 체크 로직으로 변경
    SWEA 1233. [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사

--- java/graph/1233_사칙연산_유효성_검사.java
+++ java/graph/1233_사칙연산_유효성_검사.java
@@ -2,42 +2,25 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-	static int N;
-	static String[] graph;
-	static List<String> result;
-	
-	public static boolean isOperator(String s) {
-		return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
-	}
-	
-	public static void inorder(int cur) {
-		if (graph[cur] == null)
-			return;
-		inorder(cur * 2);
-		result.add(graph[cur]);
-		inorder(cur * 2 + 1);
-	}
-	
 	public static void main(String args[]) throws Exception {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = 10;
 		
 		for(int test_case = 1; test_case <= T; test_case++) {
-			N = Integer.parseInt(br.readLine());
-			graph = new String[2 * (N+1)];
-			result = new ArrayList<>();
+			int N = Integer.parseInt(br.readLine());
+			int ans = 1;
+			
 			for (int i=0; i<N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				int num = Integer.parseInt(st.nextToken());
-				graph[num] = st.nextToken();
-			}
-			inorder(1);
-			int ans = 1;
-			for (int i=1; i<result.size()-1; i++) {
-				if (!(isOperator(result.get(i)) ^ isOperator(result.get(i+1)))) {
-					ans = 0;
-					break;
+				st.nextToken();
+				char v = st.nextToken().charAt(0);
+				if (st.hasMoreTokens()) { // 리프노드가 아닌 경우 -> 연산자
+					if (v > '0' && v < '9')
+						ans = 0;
+				}
+				else { // 리프노드인 경우 -> 숫자
+					if (v <= '0' || v > '9')
+						ans = 0;
 				}
 			}
 			System.out.printf("#%d %d\n", test_case, ans);
