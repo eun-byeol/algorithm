42f4e31162e41a3fee1a5d0a7cc8f9b69a67f92b
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-08-01 16:32:17 +0900

    fix: run 종료 조건 수정
    maxIdx == minIdx -> boxes[maxIdx] - boxes[minIdx] <= 1 조건으로 수정
    SWEA 1208. [S/W 문제해결 기본] 1일차 - Flatten

--- java/greedy/1208_Flatten.java
+++ java/greedy/1208_Flatten.java
@@ -7,7 +7,6 @@ public class Solution {
 	static int maxIdx;
 	static int minIdx;
 	
-	
 	static void findMaxMin() { // 최대 최소 인덱스 찾기
 		maxIdx = 0;
 		minIdx = 0;
@@ -24,7 +23,7 @@ public class Solution {
 	static void run(int cnt) { // 메인 로직
 		for (int i=0; i<cnt; i++) {
 			findMaxMin();
-			if (maxIdx == minIdx) return;
+			if (boxes[maxIdx] - boxes[minIdx] <= 1) return;
 			boxes[maxIdx]--;
 			boxes[minIdx]++;
 		}
@@ -32,7 +31,6 @@ public class Solution {
 	}
 	
 	public static void main(String args[]) throws Exception {
-		System.setIn(new FileInputStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
@@ -64,7 +62,7 @@ public class Solution {
 		- boxes[maxIdx]--
 		- boxes[minIdx]++
 	3) 평탄화 작업 완료 -> 횟수가 남았어도 종료
-		- 조건 : maxIdx == minIdx (가장 높은 곳과 가장 낮은 곳의 차이가 최대 1 이내)
+		- 조건 : boxes[maxIdx] - boxes[minIdx] <= 1 (가장 높은 곳과 가장 낮은 곳의 차이가 최대 1 이내)
 
 2. 결과
 	- 평탄화 수행 종료 후, 최대 최소 인덱스를 다시 찾는다
