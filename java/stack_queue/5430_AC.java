15c4686147737761a7baf571c999b44d64f28137
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-08-20 00:27:02 +0900

    feat: 5430번 AC 솔루션
    int 배열, 투포인터로 구현
    백준 https://www.acmicpc.net/problem/5430

---
+++ java/stack_queue/5430_AC.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[] arr = new int[100000]; // 배열 풀 사용
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc=0; tc<T; tc++) {
+			String p = br.readLine();
+			int n = Integer.parseInt(br.readLine());
+			
+			StringTokenizer st = new StringTokenizer(br.readLine(), "[,]");
+			
+			for (int i=0; i<n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int dir = 1;
+			int left = 0;
+			int right = n-1;
+			boolean errorFlag = false;
+			
+			for (int i=0; i<p.length(); i++) {
+				char cmd = p.charAt(i);
+				if (cmd == 'R') {
+					dir *= -1;
+				}
+				else if (cmd == 'D') {
+					if (left > right) {
+						errorFlag = true;
+						break;
+					}
+					if (dir == 1) {
+						left++;
+					}
+					else {
+						right--;
+					}
+				}
+			}
+			
+			if (errorFlag) {
+				sb.append("error\n");
+				continue;
+			}
+			
+			sb.append("[");
+			while (left <= right) {
+				if (dir == 1)
+					sb.append(arr[left++]);
+				else
+					sb.append(arr[right--]);
+				
+				if (left <= right)
+					sb.append(",");
+			}
+			sb.append("]\n");
+		}
+		System.out.println(sb);
+	}
+}
