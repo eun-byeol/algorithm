9fef9d039c16bcb0ee3cb09a0ce4f6ae2ad2772c
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-08-23 00:15:22 +0900

    refactor: 1759번 암호만들기 솔루션 개선
    check 메서드에서 모음자음 검증하지 않고, dfs 돌릴 때 같이 개수 세기
    백준 https://www.acmicpc.net/problem/1759

--- java/backtracking/1759_암호_만들기.java
+++ java/backtracking/1759_암호_만들기.java
@@ -11,47 +11,38 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		L = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-
+        
 		char[] alpha = new char[C];
 		
 		st = new StringTokenizer(br.readLine());
-
 		for (int i=0; i<C; i++) {
 			alpha[i] = st.nextToken().charAt(0);
 		}
 		Arrays.sort(alpha);
 		
 		sb = new StringBuilder();
-		
-		dfs(0, 0, new char[L], alpha);
+		dfs(0, 0, 0, 0, new char[L], alpha);
 		
 		System.out.println(sb);
 	}
 
-	private static void dfs(int depth, int at, char[] rst, char[] alpha) {
+	private static void dfs(int depth, int at, int mo, int ja, char[] rst, char[] alpha) {
 		if (depth == L) {
-			if (check(rst)) {
+			if (mo > 0 && ja > 1)
 				sb.append(String.valueOf(rst)).append("\n");
-			}
 			return;
 		}
 		
 		for (int i=at; i<C; i++) {
 			rst[depth] = alpha[i];
-			dfs(depth+1, i+1, rst, alpha);
+			if (isMoeum(alpha[i]))
+				dfs(depth+1, i+1, mo+1, ja, rst, alpha);
+			else
+				dfs(depth+1, i+1, mo, ja+1, rst, alpha);
 		}
 	}
 
-	private static boolean check(char[] rst) {
-		int mo = 0;
-		int ja = 0;
-		
-		for (char v : rst) {
-			if (v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u')
-				mo++;
-			else
-				ja++;
-		}
-		return mo > 0 && ja > 1;
+	private static boolean isMoeum(char v) {
+		return v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u';
 	}
 }
