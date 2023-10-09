eb9cc62055b909708fad99e7a6f742fa6c8727c0
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-10-10 01:38:26 +0900

    refactor: 5643번 키 순서 솔루션 개선
    LinkedList -> ArrayList로 구현
    그래프
    SWEA 5643. [Professional] 키 순서

--- java/graph/5643_키_순서.java
+++ java/graph/5643_키_순서.java
@@ -1,65 +1,64 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Solution {
-    
-    public static int bfs(int num, List<Integer>[] graph, int N) {
-    	int cnt = 0;
-    	Queue<Integer> q = new LinkedList<>();
-    	int[] visited = new int[N+1];
-    	visited[num] = 1;
-    	q.add(num);
-    	
-    	while (!q.isEmpty()) {
-    		int cur = q.poll();
-    		for (int next : graph[cur]) {
-    			if (visited[next] != 1) {
-    				cnt++;
-    				q.add(next);
-    				visited[next] = 1;
-    			}
-    		}
-    	}
-    	return cnt;
-    }
-    
-    public static void main(String args[]) throws Exception {
+public class Solution_5643_키순서 {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= T; test_case++) {
+        for (int tc=1; tc<=T; tc++) {
             int N = Integer.parseInt(br.readLine());
             int M = Integer.parseInt(br.readLine());
-            
-            List<Integer>[] in = new LinkedList[N+1];
-            List<Integer>[] out = new LinkedList[N+1];
-            
-            for (int i=0; i<N+1; i++) {
-                in[i] = new LinkedList<>();
-                out[i] = new LinkedList<>();
-            }
-            
-            for (int i=0; i<M; i++) {
-            	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                out[a].add(b);
-                in[b].add(a);
-            }
-            
-            int ans = 0;
-            
+
+            List<Integer>[] in = new List[N+1];
+            List<Integer>[] out = new List[N+1];
             for (int i=1; i<N+1; i++) {
-            	int total = 0;
-            	total += bfs(i, in, N);
-            	total += bfs(i, out, N);
-            	if (total == N-1) {
-            		ans++;
-            	}
+                in[i] = new ArrayList<>();
+                out[i] = new ArrayList<>();
             }
-            
-            System.out.printf("#%d %d\n", test_case, ans);
+
+            for (int i=0; i<M; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int v1 = Integer.parseInt(st.nextToken());
+                int v2 = Integer.parseInt(st.nextToken());
+                in[v2].add(v1);
+                out[v1].add(v2);
+            }
+
+            int ans = 0;
+            for (int i=1; i<N+1; i++) {
+                if (bfs(i, in, N) + bfs(i, out, N) == N-1) { // 자신의 키 위치 판별 기준 : 진입차수 + 진출차수 = N-1
+                    ans++;
+                }
+            }
+
+            sb.append("#")
+                    .append(tc)
+                    .append(" ")
+                    .append(ans)
+                    .append("\n");
         }
-    } 
+        System.out.print(sb);
+    }
+
+    private static int bfs(int start, List<Integer>[] graph, int N) {
+        int degree = 0;
+        int[] visited = new int[N+1];
+        Queue<Integer> q = new ArrayDeque<>();
+        visited[start] = 1;
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int cur = q.remove();
+            for (int nxt : graph[cur]) {
+                if (visited[nxt] == 0) {
+                    degree++;
+                    visited[nxt] = 1;
+                    q.add(nxt);
+                }
+            }
+        }
+        return degree;
+    }
 }
