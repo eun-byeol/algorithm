7e9fc0c91fae91899787aac960c821cda85c38bb
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2023-10-10 14:34:57 +0900

    feat: 2636번 치즈 솔루션
    구현, bfs
    백준 https://www.acmicpc.net/problem/2636

---
+++ java/implementation/2636_치즈.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_2636_치즈 {
+	static int N, M;
+	static int[][] board;
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		board = new int[N][M];
+		
+		int cnt = 0;
+		
+		for (int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j=0; j<M; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				if (board[i][j] == 1) cnt++;
+			}
+		}
+		
+		int time = 0;
+		while (cnt > 0) {
+			time++;
+			int[][] cp = copyBoard(N, M, board);
+			int remove = bfs(0, 0, cp);
+			if (cnt - remove <= 0) break;
+			cnt -= remove;
+		}
+		
+		System.out.println(time);
+		System.out.println(cnt);
+	}
+
+	private static int bfs(int r, int c, int[][] pre) {
+		Queue<int[]> q = new ArrayDeque<>();
+		int[][] visited = new int[N][M];
+		q.add(new int[] {r, c});
+		visited[r][c] = 1;
+		int removeCnt = 0;
+		
+		while(!q.isEmpty()) {
+			int[] cur = q.remove();
+			
+			for (int i=0; i<4; i++) {
+				int nx = cur[0] + dx[i];
+				int ny = cur[1] + dy[i];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M || visited[nx][ny] == 1) continue;
+				
+				if (pre[nx][ny] == 0) { // 녹은 곳인 경우
+					q.add(new int[] {nx, ny});
+				}
+				if (pre[nx][ny] == 1) { // 곧 녹아 없어지는 칸
+					board[nx][ny] = 0;
+					removeCnt++;
+				}
+				visited[nx][ny] = 1;
+			}
+		}
+		
+		return removeCnt;
+	}
+
+	private static int[][] copyBoard(int N, int M, int[][] board) {
+		int[][] cp = new int[N][M];
+		for (int i=0; i<N; i++) {
+			cp[i] = board[i].clone();
+		}
+		return cp;
+	}
+
+}
