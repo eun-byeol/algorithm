94bfb752573dddaf44cb1db62b20cbea0ce1c21c
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2024-11-03 23:06:50 +0900

    feat: PG_L2_12913_땅따먹기
    구현에서 틀림
    https://school.programmers.co.kr/learn/courses/30/lessons/12913

---
+++ java/dp/PG_L2_12913_땅따먹기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        int N = land.length;
+        
+        for (int i=1; i<N; i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][1], Math.max(land[i-1][0], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][0]));
+        }
+
+        return Math.max(land[N-1][0], Math.max(land[N-1][1], Math.max(land[N-1][2], land[N-1][3])));
+    }
+}
