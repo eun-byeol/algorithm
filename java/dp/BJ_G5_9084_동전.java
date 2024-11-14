453ba5c3d1077df704dbf9513f3a3fa0ee20c482
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2024-11-15 00:16:20 +0900

    refactor: BJ G5 9084 동전
    1차원 dp[]로 개선
    https://www.acmicpc.net/problem/9084

--- java/dp/BJ_G5_9084_동전.java
+++ java/dp/BJ_G5_9084_동전.java
@@ -22,21 +22,16 @@ class Main {
         System.out.println(sb);
     }
 
-    static long solve(int N, int[] coins, int M) {
-        long[][] dp = new long[N+1][M+1];
-        for (int i=1; i<=N; i++) {
-            dp[i][0] = 1;
-        }
+    static int solve(int N, int[] coins, int M) {
+        int[] dp = new int[M+1];
+        dp[0] = 1;
 
-        for (int i=1; i<=N; i++) {
-            int coin = coins[i-1];
-            for (int j=1; j<=M; j++) { // coin부터 시작하면 1~coin 구간의 값이 누적되지 않기 때문에, 1부터 시작!
-                if (j-coin >= 0) {
-                    dp[i][j] += dp[i][j-coin];
-                }
-                dp[i][j] += dp[i-1][j];
+        for (int i=0; i<N; i++) {
+            int coin = coins[i];
+            for (int j=coin; j<=M; j++) {
+                dp[j] += dp[j-coin];
             }
         }
-        return dp[N][M];
+        return dp[M];
     }
 }
